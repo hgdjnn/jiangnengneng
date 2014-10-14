4fb8019f0b841ebc6816791fff12d6ab13c37e01
hgdjnn <2623628929@qq.com>
2014-10-14 21:11:02 +0800

    lab2.1

--- src/action/DaoCon.java
+++ src/action/DaoCon.java
@@ -4,7 +4,7 @@ import java.sql.*;
 
 public class DaoCon {
 	public Connection GetConn() throws Exception{
-		//数据库驱动
+		//数据库
 		Class.forName("com.mysql.jdbc.Driver");
 		String url = "jdbc:mysql://localhost:3306/bookdb?characterEncodeing="+"utf-8";
 		String user = "root";
